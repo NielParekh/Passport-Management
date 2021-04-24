f27dd4f9c7b6151cc38327afe42b06af9cb1109d
Niel Parekh <55613531+NielParekh@users.noreply.github.com>
2021-04-25 02:31:24 +0530

    Add files via upload

--- check_status.py
+++ check_status.py
@@ -1,39 +1,27 @@
 from mysql.connector import connect, Error
 
-try:
-  with connect(
-          host="localhost",
-          user="root",
-          password="shrimad01",
-          database="passport"
-  )as connection:
-    view_records= "SELECT * FROM users"
-    with connection.cursor() as cursor:
-        cursor.execute(view_records)
-        result = cursor.fetchall()
-except Error as e:
-  print(e)
 
-access=1
-flag=1
-counter=0
-username = input("Enter username : ")
-for i in result:
-    if i[2]==username:
-        flag=0
-        break
-    counter+=1
-if flag==0:
-    password=input("Enter password : ")
-    if password == result[counter][3]:
-        access=0
-    else:
-        print("access denied")
-else:
-    print("User not found as a user")
-if access==0:
-    print("")
-    print("Administrator verification : " + result[counter][7])
-    print("Police verification : " + result[counter][8])
-
-
+def check_status_func(username, password):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="shrimad01",
+                database="passport"
+        )as connection:
+            view_records = "SELECT * FROM users"
+            with connection.cursor() as cursor:
+                cursor.execute(view_records)
+                result = cursor.fetchall()
+    except Error as e:
+        print(e)
+    flag = 1
+    counter = 0
+    username_local = username
+    password_local = password
+    for i in result:
+        if i[2] == username_local and i[3] == password_local:
+            flag = 0
+            break
+        counter += 1
+    return i[7], i[8], flag
--- passport.py
+++ passport.py
@@ -4,6 +4,9 @@ from admin_login import *
 from user_register import *
 from find_user import *
 from police_verify import *
+from admin_verify import *
+from user_login import *
+from check_status import *
 
 app = Flask(__name__)
 
@@ -40,18 +43,18 @@ def police_login():
             return "invalid"
 
 
-user_details = []
+user_details_police = []
 
 
 @app.route("/police_user_search", methods=["POST", "GET"])
 def police_user_search():
     if request.method == "GET":
         return render_template("police_user_search.html")
-        user_details.clear()
+        user_details_police.clear()
     else:
         user_id_user = request.form.get('user_id')
         user_exists = find_user(user_id_user)
-        user_details.append(user_exists)
+        user_details_police.append(user_exists)
         if user_exists[1] == 0:
             return redirect(url_for("police_verify"))
         else:
@@ -61,13 +64,16 @@ def police_user_search():
 @app.route("/police_verify", methods=["POST", "GET"])
 def police_verify():
     if request.method == "GET":
-        return render_template("user_details_display.html", user_id=user_details[0][0][0], username=user_details[0][0][2],address=user_details[0][0][6], email=user_details[0][0][4], aadhar=user_details[0][0][5])
+        return render_template("user_details_display_police.html", user_id=user_details_police[0][0][0],
+                               username=user_details_police[0][0][2], address=user_details_police[0][0][6],
+                               email=user_details_police[0][0][4], aadhar=user_details_police[0][0][5])
     else:
         if request.form.get('submit_button') == "yes":
-            police_verify_status = police_verify_func(user_details[0][0][0])
+            police_verify_status = police_verify_func(user_details_police[0][0][0])
             return f"{police_verify_status}"
         else:
             return "NO"
+
 
 @app.route("/admin_login", methods=["POST", "GET"])
 def admin_login():
@@ -77,7 +83,42 @@ def admin_login():
         username = request.form.get("username")
         password = request.form.get("password")
         login_status_admin = admin_login_func(username, password)
-        return f"{login_status_admin[0]}"
+        if login_status_admin[1] == 0:
+            return redirect(url_for("admin_user_search"))
+        else:
+            return "invalid"
+
+
+user_details_admin = []
+
+
+@app.route("/admin_user_search", methods=["POST", "GET"])
+def admin_user_search():
+    if request.method == "GET":
+        return render_template("admin_user_search.html")
+        user_details_admin.clear()
+    else:
+        user_id_user = request.form.get('user_id')
+        user_exists = find_user(user_id_user)
+        user_details_admin.append(user_exists)
+        if user_exists[1] == 0:
+            return redirect(url_for("admin_verify"))
+        else:
+            return "user does not exist"
+
+
+@app.route("/admin_verify", methods=["POST", "GET"])
+def admin_verify():
+    if request.method == "GET":
+        return render_template("user_details_display_admin.html", user_id=user_details_admin[0][0][0],
+                               username=user_details_admin[0][0][2], address=user_details_admin[0][0][6],
+                               email=user_details_admin[0][0][4], aadhar=user_details_admin[0][0][5])
+    else:
+        if request.form.get('submit_button') == "yes":
+            admin_verify_status = admin_verify_func(user_details_admin[0][0][0])
+            return f"{admin_verify_status}"
+        else:
+            return redirect(url_for("login"))
 
 
 @app.route("/user_choices", methods=["POST", "GET"])
@@ -85,9 +126,25 @@ def user_choices():
     if request.form.get('submit_button') == "register":
         return redirect(url_for("user_registration"))
     elif request.form.get('submit_button') == "check_status":
-        return "<h1>O<\h1>"
+        return redirect(url_for("user_login"))
     else:
         return render_template("user_options.html")
+
+
+@app.route("/user_login", methods=["POST", "GET"])
+def user_login():
+    if request.method == "GET":
+        return render_template("user_login.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        login_status_user = user_login_func(username, password)
+        if login_status_user[1] == 0:
+            user_status_details = check_status_func(username, password)
+            return render_template("user_status.html", admin_status=user_status_details[0],
+                                   police_status=user_status_details[1])
+        else:
+            return "invalid"
 
 
 @app.route("/user_registration", methods=["POST", "GET"])
